e3559e336fdc9f328c8dfc18b4a4aab066bd2821
Kaaryaa Labs <ankit.c@mrright.in>
2019-09-24 17:50:40 +0530

    core changes

--- www/DeviceInfo.js
+++ www/DeviceInfo.js
@@ -1,14 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.getImei = function(onSuccess, onError) {
-   exec(onSuccess, onError, "DeviceInfo", "getImei", []);
-};
-module.exports.getImsi = function(onSuccess, onError) {
-    exec(onSuccess, onError, "DeviceInfo", "getImsi", []);
- };
- module.exports.getIccid = function(onSuccess, onError) {
-    exec(onSuccess, onError, "DeviceInfo", "getIccid", []);
- };
- module.exports.getMac = function(onSuccess, onError) {
-    exec(onSuccess, onError, "DeviceInfo", "getMac", []);
- };
+module.exports.getImei = function (arg0, success, error) {
+    exec(success, error, 'DeviceInfo', 'getImei', [arg0]);
+}
